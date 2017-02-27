5e810792a459031fecbad5fb3351b93fd8bea68f
S1510237010 <hannes@hanness-mbp.home>
2017-02-27 10:38:46 +0100

    Adding Recipe activity, pod file, pod

---
+++ CookingApp/Header.h
@@ -0,0 +1,16 @@
+//
+//  Header.h
+//  CookingApp
+//
+//  Created by Hannes on 27.02.17.
+//  Copyright © 2017 Eva Jobst. All rights reserved.
+//
+
+#import "Parse.h"
+
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform:ios, '8.0'
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'CookingApp' do
+pod 'ActionSheetPicker-3.0'
+
+end
